3e09cba6d22aed3b4debee8dc567ece23ea360af
Vedant Goradiya <Vedntgoradiya1998@gmail.com>
2025-02-07 18:17:46 -0500

    Feat: Added Header compoent and disney image to it

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ import {
 } from "react-router-dom";
 import "./App.css";
 import { Login } from "./components/Login";
+import { Header } from "./components/Header";
 
 function App() {
   return (
     <div className="App">
       <Router>
+        <Header />
         <Routes>
           <Route path="/" element={<Login />} />
         </Routes>
