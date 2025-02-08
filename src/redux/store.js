333d2d0d901d1620cb228a641a6b0a8f37e1d603
Vedant Goradiya <Vedntgoradiya1998@gmail.com>
2025-02-08 15:45:27 -0500

    Feat: Created movie slice and added to store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "./user/userSLice";
+import movieReducer from './movies/moviesSlice'
 
 export default configureStore({
   reducer: {
     user: userReducer,
+    movie: movieReducer,
   }
 });
