bca18c3dac571228dee548f92645d5d5911e36c2
Vedant Goradiya <Vedntgoradiya1998@gmail.com>
2025-02-08 12:43:44 -0500

    Feat: Created home component

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import "./App.css";
 import { Login } from "./components/Login";
 import { Header } from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
@@ -15,6 +16,9 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Login />} />
+        </Routes>
+        <Routes>
+          <Route path="/home" element={<Home />} />
         </Routes>
       </Router>
     </div>
